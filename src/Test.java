7c2e6dc0635f64a37036d4979752ad70244e43dd
Roman <leprom@gmx.at>
2019-11-13 03:57:44 +0200

    add zusicherungen von Ana

--- src/Test.java
+++ src/Test.java
@@ -9,7 +9,7 @@ public class Test {
      *
      * Aleksa Glamocanin, e11722602@student.tuwien.ac.at : Creating Test-Data, Creating classes via UML diagram ....?
      *
-     * Ana Kubat, e01651143@student.tuwien.ac.at : Creating UML diagram for the task, Zusicherungen for ...?
+     * Ana Kubat, e01651143@student.tuwien.ac.at : Creating UML diagram for the task, Zusicherungen for all classes
      *
      * Roman Lepilov, e01529676@student.tuwien.ac.at : Creating classes via UML diagram, creating Constructors with
      * Zusicherungen
